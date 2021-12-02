8235d9b924a778e5435d46a3a738c3fd3a3f8041
Anonymous Psuedonym <fanjas112358@gmail.com>
2021-12-02 14:37:29 -0500

    git sucks

--- bot.py
+++ bot.py
@@ -1,18 +1,26 @@
 #Imports
 import discord
 import os
+import sys
 import dotenv
-import platform
 
-if platform.system() == "Windows":
-    #Probs running on my computer, load env
-    dotenv.load_dotenv()
-    #Otherwise load from heroku config_var
+from discord.ext import commands
+
+#stuff
+dotenv.load_dotenv()
     
-bot = discord.Bot()
+bot = commands.Bot()
 
+#Commands
 @bot.slash_comand()
 async def ping(ctx):
+    #ping pong
     await ctx.send("pong")
     
+@bot.slash_comand()
+async def killcr2(ctx):
+    #Kill da bot
+    sys.exit()
+    
+#R U N .
 bot.run(str(os.getenv("DISCORD_TOKEN")))
