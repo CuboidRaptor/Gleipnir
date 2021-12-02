1746214b2b2d9b46c4f98f7fd7b56b3905ff1464
Anonymous Psuedonym <fanjas112358@gmail.com>
2021-12-02 13:11:02 -0500

    git sucks

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
+#Imports
 import discord_interactions as discord
 import os
 import dotenv
+import platform
 
-dotenv.load_dotenv()
-#bot = interactions.Client(token=str(os.getenv("DISCORD_TOKEN")))
-print(str(os.getenv("DISCORD_TOKEN")))
+if platform.system() == "Windows":
+    #Probs running on my computer, load env
+    dotenv.load_dotenv()
+    #Otherwise load from heroku config_var
+    
+bot = interactions.Client(token=str(os.getenv("DISCORD_TOKEN")))
+
+@bot.command(
+    name="ping",
+    description="ping. pong. ping. pong.",
+)
+async def ping(ctx):
+    await ctx.send("pong")
