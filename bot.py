a3107e015d0f56484da05c6ad8f2b1ed363a0070
Anonymous Psuedonym <fanjas112358@gmail.com>
2021-12-02 12:13:28 -0500

    git sucks

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
-import interactions
+import discord_interactions as discord
 import os
 import dotenv
 
 dotenv.load_dotenv()
-bot = interactions.Client(token=str(os.getenv("DISCORD_TOKEN")))
+#bot = interactions.Client(token=str(os.getenv("DISCORD_TOKEN")))
+print(str(os.getenv("DISCORD_TOKEN")))
